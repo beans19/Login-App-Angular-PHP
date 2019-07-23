8c3c19bb32305e86acce5a8e654228fbc168080e
Felipe Marques <felipemarqs2015@gmail.com>
2019-07-23 04:15:04 -0300

    Add files via upload

---
+++ src/app/auth.service.ts
@@ -0,0 +1,33 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http'
+
+interface myData{
+  success: boolean,
+  message: string
+}
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+
+  loggedInStatus = false
+
+  constructor(private http: HttpClient) { }
+
+  setLoggedIn(value: boolean){
+    this.loggedInStatus = value
+  }
+
+  get isLoggedIn(){
+    return this.loggedInStatus
+  } 
+
+  getUserDetails(username, password){
+    //post in API server return user info if correct
+    return this.http.post<myData>('/api/auth.php', {
+      username,
+      password
+    })
+  }
+}
